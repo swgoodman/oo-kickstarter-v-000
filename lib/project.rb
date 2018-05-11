2bdf0d6862d42796f5392fa52dd6303612cd7714
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-11 00:08:54 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,14 @@
+
+class Project
+  attr_accessor :backers, :title
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(name)
+    @backers << name
+    name.backed_projects << self
+  end
+end
